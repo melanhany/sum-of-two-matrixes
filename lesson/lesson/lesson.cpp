29a2aa3d9e068d9cee0119c4b51c60a5e8a066f2
Melanhany <57508121+melanhany@users.noreply.github.com>
2020-11-20 22:15:58 +0500

    Add files via upload

--- lesson/lesson/lesson.cpp
+++ lesson/lesson/lesson.cpp
@@ -6,30 +6,76 @@ using namespace std;
 int main()
 {
 	int n, m, i = 0, j = 0;
+	int **a;
+	cout << "Type in elements of first matrix " << endl;
 	cin >> n;
-	int a[200][200], b[200][200], summ[200][200];
+	a = new int*[n];
+	// filling up array a with memory
+	for (i = 0; i < n; i++) {
+		a[i] = new int[n];
+	}
 	// input of elements of matrix a
 	for (i = 0; i < n; ++i)
 		for (j = 0; j < n; ++j)
 		{
-			cout << "Введите элемент а" << i + 1 << j + 1 << " : ";
+			cout << "Type in element a" << i + 1 << j + 1 << " : ";
 			cin >> a[i][j];
 		}
+
 	cin >> m;
-	cout << endl << "Введите элементы второй матрицы " << endl;
+	int **b;
+	b = new int*[m];
+	// filling up array b with memory
+	for (i = 0; i < m; i++) {
+		b[i] = new int[m];
+	}
+	cout << endl << "Type in elements of second matrix " << endl;
 	// input of elements of matrix b
 	for (i = 0; i < m; ++i) {
 		for (j = 0; j < m; ++j)
 		{
-			cout << "Введите элемент b" << i + 1 << j + 1 << " : ";
+			cout << "Type in element b" << i + 1 << j + 1 << " : ";
 			cin >> b[i][j];
 		}
 	}
-	// summarizing two matrixes
-	for (i = 0; i < n; ++i)
-		for (j = 0; j < m; ++j)
-			summ[i][j] = a[i][j] + b[i][j];
-	
+	// summarizing
+	if (n < m) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				b[i][j] += a[i][j];
+			}
+		}
+		for (int i = 0; i < m; i++, cout << endl) {
+			for (int j = 0; j < m; j++) {
+				cout << b[i][j] << " ";
+			}
+		}
+	}
+	else {
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < m; j++) {
+				a[i][j] += b[i][j];
+			}
+		}
+		for (int i = 0; i < n; i++, cout << endl) {
+			for (int j = 0; j < n; j++) {
+				cout << a[i][j] << " ";
+			}
+		}
+	}
+
+	// my mistakes
+	/*if (n == m) {
+		// summarizing two matrixes
+		for (i = 0; i < n; ++i)
+			for (j = 0; j < m; ++j)
+				summ[i][j] = a[i][j] + b[i][j];
+	}
+	else if (n < m) {
+		for (i = 0; i < n; ++i)
+			for (j = 0; j < m; ++j)
+				summ[i][j] = a[i][j] + b[i][j];
+	}
 	cout << endl << "Сумма двух матриц " << endl;
 	// output of summ
 	for (i = 0; i < n; ++i)
@@ -38,6 +84,6 @@ int main()
 			cout << summ[i][j] << "  ";
 			if (j == m - 1)
 				cout << endl;
-		}
+		}*/
 
 }
